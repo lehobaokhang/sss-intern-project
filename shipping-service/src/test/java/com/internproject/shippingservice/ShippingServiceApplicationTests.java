f0df37897ecea71cd7a18a615cc4a1321e69bef0
Khang Le <khang.le@successsoftware.global>
2023-05-26 16:31:29 +0700

    add shipping service

---
+++ shipping-service/src/test/java/com/internproject/shippingservice/ShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internproject.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
